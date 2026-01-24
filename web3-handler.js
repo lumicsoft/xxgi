faebf936e3f1d5785424ae6089562ac5e9e3fca6
lumicsoft <lumicsoft@gmail.com>
2026-01-24 16:13:35 +0530

    Update web3-handler.js

--- web3-handler.js
+++ web3-handler.js
@@ -24,6 +24,7 @@ const CONTRACT_ABI = [
     "function withdrawWorking() external",
     "function withdrawMaturity(uint256 _idx) external nonReentrant",
     "function transferFund(string _toUsername, uint256 _amount) external",
+    "function positions(address user, uint256 index) view returns (uint256 amount, uint256 startTime, uint256 lastAutoClosing, uint256 dailyRate, uint256 duration, bool isWithdrawn, bool active)",
     "function getLiveStat(address _user) view returns (uint256 totalAvailable, uint256 pendingMyROI)",
     "function getUserDashboard(address _user) view returns (tuple(uint256 totalLevelIncome, uint256 totalLevelROIIncome, uint256 totalDirectActive, uint256 totalDirectInactive, uint256 totalTeamActive, uint256 totalTeamInactive, uint256 totalIncome, uint256 totalROIIncome, uint256 totalWithdrawal, uint256 availableBalance, uint256 livePendingROI))",
     "function users(address) view returns (string username, address sponsor, uint256 directCount, uint256 workingBalance, uint256 totalWithdrawn, uint256 statLevelIncome, uint256 statLevelROIIncome, uint256 rank)",
@@ -536,6 +537,7 @@ if (window.ethereum) {
 window.addEventListener('load', () => {
     setTimeout(init, 500); 
 });
+
 
 
 
